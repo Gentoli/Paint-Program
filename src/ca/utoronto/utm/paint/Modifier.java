05dcea91cfefc469a98cbf25cd6855a136160dd7
dongshu2 <shuyi.dong@mail.utoronto.ca>
2017-11-05 18:32:07 -0500

    fixes to merge

--- src/ca/utoronto/utm/paint/Modifier.java
+++ src/ca/utoronto/utm/paint/Modifier.java
@@ -2,10 +2,9 @@ package ca.utoronto.utm.paint;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.Stroke;
 
 public class Modifier extends Shape {
-	public Modifier(int x, int y, Color colour, float lineThickness, boolean fill, Stroke stroke) {
+	public Modifier(int x, int y, Color colour, float lineThickness, boolean fill, int stroke) {
 		super(x, y, colour, lineThickness, fill, stroke);
 	}
 
