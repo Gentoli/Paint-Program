a29d03588c25d3ca3ad34f50bd2e554370c299a7
Kurtis Assad <kurtis.assad@mail.utoronto.ca>
2017-11-05 22:15:12 -0500

    fixing icons

--- src/ca/utoronto/utm/paint/ShapeButton.java
+++ src/ca/utoronto/utm/paint/ShapeButton.java
@@ -1,22 +1,25 @@
 package ca.utoronto.utm.paint;
 
-import javax.swing.JButton;
-import javax.swing.JPanel;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class ShapeButton extends JButton {
     private Shape shape;
     private int shapeNum;
     private JPanel canvas;
-
     public ShapeButton(int shapeNum) {
         this.shapeNum = shapeNum==0?5:shapeNum;
+
         this.canvas = new JPanel(){
             @Override
             public void paint(Graphics g) {
                 shape.setXEnd(canvas.getWidth()-1);
                 shape.setYEnd(canvas.getHeight()-1);
-                shape.print(g);
+//                shape.print(g);
                 g.dispose();
             }
         };
