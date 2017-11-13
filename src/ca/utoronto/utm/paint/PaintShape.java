1b7f44c9eaacc64b812166194393597916f4e865
Kurtis Assad <kurtis.assad@mail.utoronto.ca>
2017-11-12 19:56:57 -0500

    added more line styles

--- src/ca/utoronto/utm/paint/PaintShape.java
+++ src/ca/utoronto/utm/paint/PaintShape.java
@@ -2,6 +2,7 @@ package ca.utoronto.utm.paint;
 
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.QuadCurve2D;
 
 /**
  *  PaintShape base object for all shapes(Actors) on ShapePanel. Implements Drawable
@@ -25,10 +26,13 @@ public abstract class PaintShape implements Drawable, Shape {
 		this.fill = fill;
 		this.strokeStyle = strokeStyle;
 		switch (strokeStyle) {
-			case 0:  this.stroke = new StrokeType1(new Ellipse2D.Double(0,0,5.0,5.0),15.0);
+			case 0:  this.stroke = new BasicStroke(lineThickness);
 				break;
 			case 1:  this.stroke = new BasicStroke(lineThickness, 0, 0, 10.0f, new float[] {16.0f,20.0f},0.0f);
 				break;
+			case 2: this.stroke = new ShapeStroke(new QuadCurve2D.Double(-0.1,0.1,0.0,0.0,0.1,0.1),1.0,true);
+				break;
+			case 3: this.stroke = new  ShapeStroke(new QuadCurve2D.Double(50,100,100.0,170.0,150,100),1.0,false);
 			default: this.stroke = new BasicStroke(lineThickness);
 				break;
 		}
--- src/ca/utoronto/utm/paint/ShapeStroke.java
+++ src/ca/utoronto/utm/paint/ShapeStroke.java
@@ -3,17 +3,21 @@ package ca.utoronto.utm.paint;
 import java.awt.*;
 import java.awt.geom.*;
 
-public class StrokeType1 implements Stroke {
+public class ShapeStroke implements Stroke {
     private Shape[] shapes;
     private double spacing;
     private AffineTransform t = new AffineTransform();
+    private boolean serendipity;
 
-    public StrokeType1(Shape s, double spacing){this(new Shape[] {s}, spacing );}
-    public StrokeType1(Shape[] s, double spacing){
+    public ShapeStroke(Shape s, double spacing, boolean serendipity){this(new Shape[] {s}, spacing ,serendipity);}
+    public ShapeStroke(Shape[] s, double spacing, boolean serendipity){
         this.shapes = new Shape[s.length];
         this.spacing = spacing;
+        this.serendipity = serendipity;
+
         for(int i = 0; i < shapes.length; i++){
             Rectangle2D bounds = s[i].getBounds2D();//get the shape boundries so we can find middle
+            System.out.println(bounds);
             double midX = bounds.getCenterX();
             double midY = bounds.getCenterY();
             t.setToTranslation(-midX,-midY);//translates the shape into model view (symmetric along the axis)
@@ -21,7 +25,6 @@ public class StrokeType1 implements Stroke {
         }
     }
 
-    @Override
     public Shape createStrokedShape(Shape s) {
         GeneralPath path = new GeneralPath();
         PathIterator pi = new FlatteningPathIterator(s.getPathIterator(null) , 1);
@@ -29,10 +32,17 @@ public class StrokeType1 implements Stroke {
         int state = 0;
         double[] points = new double[6];
         double moveX = 0; double moveY = 0; double thisX = 0;double thisY = 0;double lastX = 0;double lastY = 0;
+        double firstX = 0, firstY = 0;
         double offset = 0;
+        boolean please = false;
 
         while( !pi.isDone() ){
             state = pi.currentSegment(points);//updates point array with the points of the lines
+            if(please){
+                firstX = points[0];
+                firstY = points[0];
+                please = true;
+            }
             if(state == PathIterator.SEG_MOVETO){
                 offset = 0;
                 moveX = lastX = points[0];
@@ -50,10 +60,11 @@ public class StrokeType1 implements Stroke {
                 double distance = Math.sqrt(Math.pow(thisX-lastX,2)+Math.pow(thisY-lastY,2));
                 double angle = Math.atan2(thisY-lastY,thisX-lastX);
                 while(distance >= offset && shapeIndex < shapes.length){
-                    double x = lastX + offset*(thisX-lastX)/distance;
-                    double y = lastY + offset*(thisY-lastY)/distance;
+                    double x = lastX + offset*Math.cos(angle);
+                    double y = lastY + offset*Math.sin(angle);
                     t.setToTranslation(x,y);
                     t.rotate(angle);
+                    path.curveTo(x-1,y-1,firstX,firstY,x+1,y+1);
                     path.append(t.createTransformedShape(shapes[shapeIndex]),false);//set the shapes position to the correct spot
                     offset += spacing;
                     shapeIndex++; shapeIndex %= shapes.length;// increments and loops if it is out of bounds
