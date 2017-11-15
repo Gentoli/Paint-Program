b35ff3a11a6bda68c2b0e5d155e3d3bd1e404603
dongshu2 <shuyi.dong@mail.utoronto.ca>
2017-11-14 23:44:46 -0500

    Docs

--- src/ca/utoronto/utm/paint/ShapeManipulatorStrategy.java
+++ src/ca/utoronto/utm/paint/ShapeManipulatorStrategy.java
@@ -3,14 +3,34 @@ package ca.utoronto.utm.paint;
 import ca.utoronto.utm.pointer.ModifierEvent;
 import ca.utoronto.utm.pointer.PointerEvent;
 
+/**
+ * Strategy each tool follow
+ */
 public interface ShapeManipulatorStrategy {
 
+	/**
+	 * Called when the tool is deselected
+	 * @return the current active drawable
+	 */
 	public Drawable deselect();
 
+	/**
+	 *  Called when the tool is selected
+	 */
 	public void selected();
 
+	/**
+	 * Called when Pointer update is received
+	 * @param e the PointerEvent
+	 * @return
+	 */
 	public Drawable handlePointerUpdate(PointerEvent e);
 
+	/**
+	 * Called when Key update is received
+	 * @param e the ModifierEvent
+	 * @return
+	 */
 	public boolean handleModifierUpdated(ModifierEvent e);
 
 }
